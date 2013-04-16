b19f3f97668dd5c34f5f79364cb008a358aa3f4c
Charles Stuart Dear <stuart.dear@intermarkgroup.com>
2013-04-16 16:25:35 -0500

    AAQform.cs Created

---
+++ sanityProject/sanity/AAQform.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace sanity
+{
+    [TestFixture]
+    
+    public class AskAQuestionForm
+    {
+        private IWebDriver driver;
+        private IWebElement webby;  
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        public static void WaitForAjaxElement(IWebDriver driver, By byElement, double timeoutSeconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+            wait.Until(x => x.FindElement(byElement));
+        }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://southeast.buyatoyota.com/";
+            verificationErrors = new StringBuilder();
+            
+         
+        }
+        
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        
+        public void TemplateFunction()
+        {
+           
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            driver.Navigate().GoToUrl("http://southeast.buyatoyota.com/");
+            Thread.Sleep(20000);
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Navigate().Refresh();
+            Thread.Sleep(5000);
+            driver.FindElement(By.LinkText("CONFIRM ZIP")).Click();
+            // Dealer Confirmed.
+            Thread.Sleep(10000);
+            driver.FindElement(By.ClassName("btn")).Click();
+            Thread.Sleep(10000);
+            driver.FindElement(By.ClassName("tab-sms-link")).Click();
+            Thread.Sleep(5000);
+            driver.FindElement(By.CssSelector("span.ui-icon.ui-icon-closethick")).Click();
+            // AAQ Form Window Loaded and Closed
+            driver.FindElement(By.CssSelector("a.tab-offers-link > span")).Click();
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Featured Offers')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+            driver.FindElement(By.CssSelector("a.tab-resources-link > span")).Click();
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Finance Tools')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            driver.Close();
+            // Content Loaded Tabs View Offers and Helpful Resources Verified
+            
+        }
+
+        //Extension Methods... 
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alert.Text;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
