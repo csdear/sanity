3c91cda49d02835a3422fb9d7631ad8be9b2ac40
Charles Stuart Dear <stuart.dear@intermarkgroup.com>
2013-04-18 13:52:11 -0500

    Added BYO section

---
+++ sanityProject/sanity/BYO.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace sanity
+{
+    [TestFixture]
+    
+    public class BuildYourOwn
+    {
+        private IWebDriver driver;
+        private IWebElement webby;  
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        public static void WaitForAjaxElement(IWebDriver driver, By byElement, double timeoutSeconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSeconds));
+            wait.Until(x => x.FindElement(byElement));
+        }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://southeast.buyatoyota.com/";
+            verificationErrors = new StringBuilder();
+            
+         
+        }
+        
+       /* [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        } */
+
+        [Test]
+        
+        public void BuildYourOwnMain()
+        {
+           
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            driver.Navigate().GoToUrl("http://southeast.buyatoyota.com/#");
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Navigate().Refresh();
+            Thread.Sleep(20000);
+            // BEGIN BUILD - BYO  - CAMRY  
+            driver.Navigate().GoToUrl("http://southeast.buyatoyota.com/camry/build");
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'SELECT A GRADE')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            Thread.Sleep(5000);
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Select A Model')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            Thread.Sleep(5000);
+
+            driver.FindElement(By.LinkText("Next")).Click();
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Exterior:')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Interior:')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            driver.FindElement(By.CssSelector("div.color-description")).Click();
+            driver.FindElement(By.CssSelector("img[alt=\"Barcelona Red Metallic\"]")).Click();
+            driver.FindElement(By.XPath("(//img[@alt='Ivory Fabric'])[7]")).Click();
+
+            try
+            {
+                Assert.IsTrue(IsElementPresent(By.CssSelector("div.vehicle-image > img")));
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+
+            driver.FindElement(By.LinkText("Next")).Click();
+            driver.FindElement(By.CssSelector("li.packages-tab.selected")).Click();
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Click on a package name for details')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            driver.FindElement(By.LinkText("Prev")).Click();
+            driver.FindElement(By.LinkText("Next")).Click();
+            driver.FindElement(By.LinkText("Next")).Click();
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Click on accessory name for details')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Wheels')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Technology')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Options')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                Assert.IsTrue(IsElementPresent(By.CssSelector("div.vehicle-image > img")));
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+
+            driver.FindElement(By.LinkText("Next")).Click();
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Required Field')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Preferred Method Of Contact')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            try
+            {
+                IWebElement el = driver.FindElement(By.XPath("//*[contains(.,'Submit')]"));
+
+            }
+
+            catch (AssertionException e)
+            {
+
+                verificationErrors.Append(e.Message);
+            }
+
+            driver.FindElement(By.CssSelector("li.options-tab")).Click();
+            driver.FindElement(By.CssSelector("li.packages-tab")).Click();
+            driver.FindElement(By.CssSelector("li.color-tab")).Click();
+            driver.FindElement(By.CssSelector("li.model-tab")).Click();
+            driver.FindElement(By.CssSelector("img.tam-image")).Click();
+            //END BYO 
+            
+            
+            driver.Close();      
+        }
+
+        //Extension Methods... 
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alert.Text;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
